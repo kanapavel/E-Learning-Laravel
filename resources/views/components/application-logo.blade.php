916e7a1db91c197e026faabe5950a22bc82cd7fc
Darios <pavelkana19@gmail.com>
2026-04-08 13:12:23 +0100

    ajout du fichier railwaiy a la racine

--- resources/views/components/application-logo.blade.php
+++
@@ -1,6 +0,0 @@
-<a href="/">
-    <svg class="w-10 h-10" viewBox="0 0 316 316" fill="none">
-        <path d="M158 0L316 79V237L158 316L0 237V79L158 0Z" fill="#0056d2"/>
-        <path d="M158 79L237 118V197L158 237L79 197V118L158 79Z" fill="white"/>
-    </svg>
-</a>
